5fbfcad1ea4d756fa2d2f698cbbcbd8e8a96155b
marciiosouza <marciiosouza@outlook.com>
2023-10-09 07:15:10 -0300

    Inserindo interação á página

--- script.js
+++ script.js
@@ -2,17 +2,46 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
 
-
-// setAttribute -> recebe dois parametros, o 1º o que você quer alterar, 2º o que inserir
+// setAttribute -> recebe dois parametros, o 1º o que você quer alterar, 2º o que inserir *define ou altera o valor do atributo
 focoBt.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'foco')
+  alterarContexto('foco')
+  focoBt.classList.add('active')
 })
 
 curtoBt.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-curto')
+  alterarContexto('descanso-curto')
+  curtoBt.classList.add('active')
 })
 
 longoBt.addEventListener('click', () => {
-  html.setAttribute('data-contexto', 'descanso-longo')
+  alterarContexto('descanso-longo')
+  longoBt.classList.add('active')
 })
+
+function alterarContexto(contexto) {
+  botoes.forEach(function (contexto) {
+    contexto.classList.remove('active')
+  })
+  html.setAttribute('data-conexto', contexto)
+  banner.setAttribute('src', `/imagens/${contexto}.png`)
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = 
+      `Otimize sua produtividade,<br> <strong class="app__title-strong">mergulhe no que importa.</strong>`
+      break;
+    case "descanso-curto":
+      titulo.innerHTML = `
+        Que tal dar uma respirada?<br>
+        <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+      break;
+    case "descanso-longo":
+      titulo.innerHTML = `
+        Hora de voltar à superfície.<br>
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+      break;
+  }
+}
